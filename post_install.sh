cddf69717a4a6be48f2470a587c83257c3a88883
trevorijones <trevorijones@yahoo.com>
2019-04-03 04:35:46 -0700

    Post installs update

--- post_install.sh
+++ post_install.sh
@@ -2,21 +2,20 @@
 VIM_RUNTIME_DIR="$(dirname "$(realpath $0)")"
 
 utils_curlsha256() {
-    local url=$1
-    local url_sha=$2
-    if [ "200" == "$(curl -L -s -w "%{http_code}" -O $url)" ]; then
-        if [ "200" == "$(curl -L -s -w "%{http_code}" -O $url_sha)" ]; then
-            grep ${url##*/} ${url_sha##*/} | sha256sum -c -
-        else
-            echo "Failed to fetch $url_sha"
-			return 1
-        fi
+  local url=$1
+  local url_sha=$2
+  if [ "200" == "$(curl -L -s -w "%{http_code}" -O $url)" ]; then
+    if [ "200" == "$(curl -L -s -w "%{http_code}" -O $url_sha)" ]; then
+      grep ${url##*/} ${url_sha##*/} | sha256sum -c -
     else
-        echo "Failed to fetch $url"
-		return 1
+      echo "Failed to fetch $url_sha"
+      return 1
     fi
+  else
+    echo "Failed to fetch $url"
+    return 1
+  fi
 }
-
 
 install_global_go() {
   local -
@@ -43,42 +42,41 @@ install_shellcheck() {
 
 # Syntastic java checker
 install_java_checkstyle() {
-    curl --insecure -L -O https://github.com/checkstyle/checkstyle/releases/download/checkstyle-8.18/checkstyle-8.18-all.jar
-    curl --insecure -L -O https://raw.githubusercontent.com/checkstyle/checkstyle/master/src/main/resources/google_checks.xml
+  curl --insecure -L -O https://github.com/checkstyle/checkstyle/releases/download/checkstyle-8.18/checkstyle-8.18-all.jar
+  curl --insecure -L -O https://raw.githubusercontent.com/checkstyle/checkstyle/master/src/main/resources/google_checks.xml
 }
 
-install_node(){
-    local version=v8.11.2
-    local url_sha="https://nodejs.org/dist/${version}/SHASUMS256.txt"
-	local url="https://nodejs.org/dist/$version/node-${version}-linux-x64.tar.xz"
+install_node() {
+  local version=v8.11.2
+  local url_sha="https://nodejs.org/dist/${version}/SHASUMS256.txt"
+  local url="https://nodejs.org/dist/$version/node-${version}-linux-x64.tar.xz"
 
-	[ -d "$VIM_RUNTIME_DIR/opt" ] || mkdir "$VIM_RUNTIME_DIR/opt"
-	(cd "$VIM_RUNTIME_DIR/opt" &&
-        utils_curlsha256 $url $url_sha &&
-        tar Jxf node* && rm *.tar.xz SHASUMS* && mv node* node)
+  [ -d "$VIM_RUNTIME_DIR/opt" ] || mkdir "$VIM_RUNTIME_DIR/opt"
+  (cd "$VIM_RUNTIME_DIR/opt" &&
+    utils_curlsha256 $url $url_sha &&
+    tar Jxf node* && rm *.tar.xz SHASUMS* && mv node* node)
 }
 
 # use to generate the node project
 install_node_utils() {
-    (
+  (
     NODE_HOME="$VIM_RUNTIME_DIR/opt/node"
     PATH="$PATH:$NODE_HOME/bin"
     node $NODE_HOME/bin/npm install -g prettier
-    )
+  )
 }
 
 if [ -n "$1" ]; then
-    if declare -F | cut -d' ' -f 3 | grep -q "^${1}$"; then
-        "$1"
-    fi
+  if declare -F | cut -d' ' -f 3 | grep -q "^${1}$"; then
+    "$1"
+  fi
 else
 
-
-go version || install_global_go
-install_local_shfmt
-install_shellcheck
-install_java_checkstyle
-sudo pip install requests
-./update_plugins.py
+  go version || install_global_go
+  install_local_shfmt
+  install_shellcheck
+  install_java_checkstyle
+  sudo pip install requests
+  ./update_plugins.py
 
 fi
--- update_plugins.py
+++ update_plugins.py
@@ -14,7 +14,6 @@ import requests
 
 from os import path
 
-
 #--- Globals ----------------------------------------------
 PLUGINS = """
 vim-yankstack https://github.com/maxbrunsfeld/vim-yankstack
@@ -75,8 +74,8 @@ def download_extract_replace(plugin_name, zip_path, temp_dir, source_dir):
     zip_f = zipfile.ZipFile(temp_zip_path)
     zip_f.extractall(temp_dir)
 
-    plugin_temp_path = path.join(temp_dir,
-                                 path.join(temp_dir, '%s-master' % plugin_name))
+    plugin_temp_path = path.join(
+        temp_dir, path.join(temp_dir, '%s-master' % plugin_name))
 
     # Remove the current plugin and replace it with the extracted
     plugin_dest_path = path.join(source_dir, plugin_name)
@@ -94,8 +93,7 @@ def download_extract_replace(plugin_name, zip_path, temp_dir, source_dir):
 def update(plugin):
     name, github_url = plugin.split(' ')
     zip_path = GITHUB_ZIP % github_url
-    download_extract_replace(name, zip_path,
-                             temp_directory, SOURCE_DIR)
+    download_extract_replace(name, zip_path, temp_directory, SOURCE_DIR)
 
 
 if __name__ == '__main__':
